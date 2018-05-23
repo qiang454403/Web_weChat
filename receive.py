b473f5a27d9e8959b06587f855f8e65b6b8cf46c
gouzhiqiang <zhiqiang.gou@ceeyoo.net>
2018-05-23 15:40:49 +0800

    re

--- receive.py
+++ receive.py
@@ -1,4 +1,6 @@
-# -*- coding: utf-8 -*-# filename: receive.pyimport xml.etree.ElementTree as ETdef parse_xml(web_data):
+# -*- coding: utf-8 -*-# filename: receive.py
+import xml.etree.ElementTree as ETdef parse_xml(web_data):
+
 if len(web_data) == 0:
 	return None
 xmlData = ET.fromstring(web_data)
