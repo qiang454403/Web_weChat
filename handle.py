571fc1f035d357abab2da12e87f58f3fe67c9698
gouzhiqiang <zhiqiang.gou@ceeyoo.net>
2018-05-23 14:23:23 +0800

    msg test

--- handle.py
+++ handle.py
@@ -1,30 +1,18 @@
-# -*- coding: utf-8 -*-
-# filename: handle.py
-
-import hashlib
-import web
-
-class Handle(object):
-    def GET(self):
+# -*- coding: utf-8 -*-# filename: handle.pyimport hashlibimport replyimport receiveimport webclass Handle(object):
+    def POST(self):
         try:
-            data = web.input()
-            if len(data) == 0:
-                return "hello, this is handle view"
-            signature = data.signature
-            timestamp = data.timestamp
-            nonce = data.nonce
-            echostr = data.echostr
-            token = "ec57a59b05bb71d33" #请按照公众平台官网\基本配置中信息填写
-
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
-            print "handle/GET func: hashcode, signature: ", hashcode, signature
-            if hashcode == signature:
-                return echostr
+            webData = web.data()
+            print "Handle Post webdata is ", webData
+   #后台打日志
+            recMsg = receive.parse_xml(webData)
+            if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                content = "test"
+                replyMsg = reply.TextMsg(toUser, fromUser, content)
+                return replyMsg.send()
             else:
-                return ""
-        except Exception, Argument:
-            return Argument
+                print "暂且不处理"
+                return "success"
+        except Exception, Argment:
+            return Argment
