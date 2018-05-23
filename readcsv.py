134070ace628f1a1e635082fb1f404557426eed8
gzq <272293248@qq.com>
2018-05-23 20:00:31 +0800

    lunux csv

--- readcsv.py
+++ readcsv.py
@@ -1,7 +1,7 @@
-#coding=gbk
+#coding=utf-8
 import codecs
 f = codecs.open('doc/Item.csv','r','gbk')
 s = f.readlines()
 f.close()
 for line in s:
-    print line.encode('gbk')
+    print line.encode('utf-8')
