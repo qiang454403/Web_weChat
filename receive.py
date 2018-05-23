2d17bbc1e2297e99d39519d10f3c5b2d2f1fb616
gouzhiqiang <zhiqiang.gou@ceeyoo.net>
2018-05-23 15:46:16 +0800

    re

--- receive.py
+++ receive.py
@@ -8,17 +8,19 @@ msg_type = xmlData.find('MsgType').text
 if msg_type == 'text':
 	return TextMsg(xmlData)
 elif msg_type == 'image':
-	return ImageMsg(xmlData)class Msg(object):
-def __init__(self, xmlData):
-	self.ToUserName = xmlData.find('ToUserName').text
-	self.FromUserName = xmlData.find('FromUserName').text
-	self.CreateTime = xmlData.find('CreateTime').text
-	self.MsgType = xmlData.find('MsgType').text
-	self.MsgId = xmlData.find('MsgId').textclass TextMsg(Msg):
-def __init__(self, xmlData):
-	Msg.__init__(self, xmlData)
-	self.Content = xmlData.find('Content').text.encode("utf-8")class ImageMsg(Msg):
-def __init__(self, xmlData):
-	Msg.__init__(self, xmlData)
-	self.PicUrl = xmlData.find('PicUrl').text
-	self.MediaId = xmlData.find('MediaId').text
+	return ImageMsg(xmlData)
+class Msg(object):
+	def __init__(self, xmlData):
+		self.ToUserName = xmlData.find('ToUserName').text
+		self.FromUserName = xmlData.find('FromUserName').text
+		self.CreateTime = xmlData.find('CreateTime').text
+		self.MsgType = xmlData.find('MsgType').text
+		self.MsgId = xmlData.find('MsgId').textclass TextMsg(Msg):
+	def __init__(self, xmlData):
+		Msg.__init__(self, xmlData)
+		self.Content = xmlData.find('Content').text.encode("utf-8")
+class ImageMsg(Msg):
+	def __init__(self, xmlData):
+		Msg.__init__(self, xmlData)
+		self.PicUrl = xmlData.find('PicUrl').text
+		self.MediaId = xmlData.find('MediaId').text
