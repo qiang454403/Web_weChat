0a1eba3347589a127bb568f8089cedf254bd58a8
jack <gzqxglove@126.com>
2018-05-27 23:34:55 +0800

    aa

--- reply.py
+++ reply.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-# filename: reply.py
 import time
+import sys 
 
 class Msg(object):
 	def __init__(self):
@@ -14,16 +15,22 @@ class TextMsg(Msg):
 		self.__dict['CreateTime'] = int(time.time())
 		self.__dict['Content'] = content
 	def send(self):
-		XmlForm = """
-		<xml>
-		<ToUserName><![CDATA[{ToUserName}]]></ToUserName>
-		<FromUserName><![CDATA[{FromUserName}]]></FromUserName>
-		<CreateTime>{CreateTime}</CreateTime>
-		<MsgType><![CDATA[text]]></MsgType>
-		<Content><![CDATA[{Content}]]></Content>
-		</xml>
-		"""
-		return XmlForm.format(**self.__dict)
+		#XmlForm = """
+		#<xml>
+		#<ToUserName><![CDATA[{ToUserName}]]></ToUserName>
+		#<FromUserName><![CDATA[{FromUserName}]]></FromUserName>
+		#<CreateTime>{CreateTime}</CreateTime>
+		#<MsgType><![CDATA[text]]></MsgType>
+		#<Content><![CDATA[{Content}]]></Content>
+		#</xml>
+		#"""
+		XmlForm = "<xml>"
+		XmlForm=XmlForm+"<ToUserName><![CDATA["+self.__dict['ToUserName']+"]]></ToUserName>"
+		XmlForm=XmlForm+"<FromUserName><![CDATA["+self.__dict['FromUserName']+"]]></FromUserName>"
+		XmlForm=XmlForm+"<CreateTime>"+"111111111"+"</CreateTime>"
+		XmlForm=XmlForm+"<MsgType><![CDATA[text]]></MsgType><Content><![CDATA["
+		XmlForm = XmlForm+self.__dict['Content']+"]]></Content></xml>"
+		return XmlForm#.format(**self.__dict)
 class ImageMsg(Msg):
 	def __init__(self, toUserName, fromUserName, mediaId):
 		self.__dict = dict()
